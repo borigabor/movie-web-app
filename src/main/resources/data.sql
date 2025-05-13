9d63253414a42280e44afcf5fad3bf203ed947d0
borigabor <borigabor07@gmail.com>
2025-05-13 09:24:37 +0200

    Add unit tests for controllers

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,8 +8,8 @@ values ('31236118-7dbb-4343-adc2-d39a0faceeff',
 
 insert into director(id, avatar, name, nationality, date_of_birth)
 values ('1f71198d-6cc0-41f6-aa4b-7aef693a2923',
-        'https://m.media-amazon.com/images/M/MV5BMjMxMDQ0NDk3MV5BMl5BanBnXkFtZTgwMjEzMDE4NzE@._V1_.jpg',
-        'John Krasinski',
+        'https://m.media-amazon.com/images/M/MV5BMTA1MDY5ODcxMjBeQTJeQWpwZ15BbWU3MDk0ODUyNDI@._V1_.jpg',
+        'David Leitch',
         'Newton, Massachusetts, USA',
         '1979-10-20'
         );
@@ -28,12 +28,24 @@ VALUES ('9ae27569-2e6e-4cb1-84b7-12a2e8958e84',
 
 INSERT INTO movie(id, cover_url, title, release_date, lang, duration, genre, plot, director_id)
 VALUES ('67ecddeb-87d6-4183-815b-c529a84bb9c8',
-        'https://m.media-amazon.com/images/M/MV5BYWY2YzRlOWMtMTU1OC00Y2Y1LWJhNGQtZmM3MDU4MDE3NTUxXkEyXkFqcGc@._V1_.jpg',
-        'A Quiet Place',
-        '2016-10-05',
+        'https://m.media-amazon.com/images/M/MV5BM2U0MTJiYTItMjNiZS00MzU4LTkxYTAtYTU0ZGY1ODJhMjRhXkEyXkFqcGc@._V1_.jpg',
+        'The Fall Guy',
+        '2024-10-05',
         'English',
-        90,
+        126,
+        'Comedy',
+        'A stuntman, fresh off an almost career-ending accident, has to track down a missing movie star, solve a conspiracy and try to win back the love of his life while still doing his day job.',
+        '1f71198d-6cc0-41f6-aa4b-7aef693a2923'
+        );
+
+INSERT INTO movie(id, cover_url, title, release_date, lang, duration, genre, plot, director_id)
+VALUES ('3d3cbe96-2308-46d4-a02c-7584c98a50d1',
+        'https://m.media-amazon.com/images/M/MV5BMWJiNjJmOWYtMzNmNi00NzM3LTkzYTgtOWMzNTJmMDIyOTVlXkEyXkFqcGc@._V1_.jpg',
+        'Atomic Blonde',
+        '2017-04-20',
+        'English',
+        115,
         'Drama',
-        'A family struggles for survival in a world invaded by blind alien creatures with ultra-sensitive hearing.',
+        'An undercover MI6 agent is sent to Berlin during the Cold War to investigate the murder of a fellow agent and recover a missing list of double agents.',
         '1f71198d-6cc0-41f6-aa4b-7aef693a2923'
         );
