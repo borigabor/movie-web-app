5a6559261a522cfb3532268cde72090024d6e0ab
borigabor <borigabor07@gmail.com>
2025-05-23 14:54:36 +0200

    test commit

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -17,7 +17,7 @@ values ('1f71198d-6cc0-41f6-aa4b-7aef693a2923',
 INSERT INTO movie(id, cover_url, title, release_date, lang, duration, genre, plot, director_id)
 VALUES ('9ae27569-2e6e-4cb1-84b7-12a2e8958e84',
         'https://m.media-amazon.com/images/M/MV5BMDU0NjcwOGQtNjNjOS00NzQ3LWIwM2YtYWVmODZjMzQzN2ExXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg',
-        'Alien: Romolus',
+        'Alien: Romolos',
         '2024-07-15',
         'English',
         119,
